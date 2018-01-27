3fbffaa113635a299592175bec3a013f65c61fed
Stephen Agwu <osa10928@gmail.com>
2018-01-26 18:49:42 -0600

    added http connection for movies (not movie)

--- src/app/movie.service.ts
+++ src/app/movie.service.ts
@@ -1,17 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
 import { of } from 'rxjs/observable/of'
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { Movie } from './classes/movie';
 import { MOVIES } from './mock-movies';
 
 @Injectable()
 export class MovieService {
+  moviesUrl = 'api/movies';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   getMovies () {
-    return of(MOVIES)
+    return this.http.get<Movie[]>(this.moviesUrl)
   }
 
   getMovie (title:string, year:number): Observable<Movie> {
